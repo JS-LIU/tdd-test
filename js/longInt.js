6ad20fbd4bfdc7c7a829710fc1e330b561f53129
JS-LIU <984003917@qq.com>
2015-07-01 18:02:06 +0800

    你妈比老子去吃饭什么特么tdd 老子还是不懂 你妈比该不会还是不会

--- js/longInt.js
+++ js/longInt.js
@@ -8,19 +8,24 @@
     console.log(multiply('4','5'));
 }());
 (function test_47_multiply_5(){
-    console.log(multiply('999','5'));
+    console.log(multiply('16','8'));
 }());
 
-(function test_22_mutiply_22(){
+(function test_23_mutiply_21(){
 	console.log(multiply('23','21'));
 }());
+
+(function test_16_mutiply_18(){
+	console.log(multiply('16','18'));
+}());
+
 
 function multiply(multA,multB){
 	 
 		
 
-	resultStringOld = '';
-	resultt = '';
+	var resultStringOld = '',
+		cover = '';
 	for(var i = multB.length -1; i >= 0; i--){
 		var b = multB[i],
 			carry = 0,
@@ -34,23 +39,35 @@ function multiply(multA,multB){
 			rem = result % 10 + carry;
 	
 			carry = parseInt(result / 10);
-			resultStringNew += int_string(rem);
+			var string_rem = int_string(rem);
 			
+			string_rem = string_rem.split('').reverse().join('');
+			resultStringNew += string_rem;
 		}
 		
 		if(int_string(carry)!=0){
 			resultStringNew = (resultStringNew += int_string(carry));
 		}
-		if(multB.length > 1){
-			var resultStringOld = resultStringNew;
+		resultStringNew = resultStringNew.split('').reverse().join('');
+		
+		if(i == multB.length -1){
+			resultStringOld = resultStringNew;
+		}else{
+			
+			cover = cover +'0';			
+			resultStringNew = resultStringNew + cover;
+			if(resultStringNew.length > resultStringOld.length){
+				resultStringOld = '0' + resultStringOld;
+			}
+
+			for(var k = resultStringOld.length-1;k >=0; k--){
+				resultStringOld = resultStringOld.split('');	
+				resultStringOld[k] = int_string(parseInt(resultStringNew[k]) + parseInt(resultStringOld[k]));
+				resultStringOld = resultStringOld.join('');
+			}
 		}
-		if(i > 0){
-			
-			
-		}				
 	}
-	
-	return resultt;
+	return resultStringOld;
 	
 };
 
